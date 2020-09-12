62aaa58e9d534ed4a3b76fce82706130a1e5cfc9
ParthJ39 <55129071+ParthJ39@users.noreply.github.com>
2020-09-12 19:49:17 +0530

    updating reducer.js file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger'; 
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
